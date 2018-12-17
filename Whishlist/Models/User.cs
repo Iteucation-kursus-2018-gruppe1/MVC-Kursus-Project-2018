cb0d239808d0cebc20f2757993d02df4aa4631e2
Dag Bechmann Andersen <kallmagan@hotmail.com>
2018-12-17 11:49:25 +0100

    Added Other Files Folder

---
+++ Whishlist/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Whishlist.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password{ get; set; }
+    }
+}
